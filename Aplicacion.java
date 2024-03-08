dbdbef4fb0abfe0df9afe99621400baf8a62fa78
GSukia <113173930+GSukia@users.noreply.github.com>
2024-03-08 11:06:33 +0100

    commit añado Aplicacion
    Añado el fichero Aplicacion.java que suma dos cifras.

---
+++ Aplicacion.java
@@ -0,0 +1,17 @@
+package hoja1;
+
+// Declara dos cifras, los suma y muestra el resultado
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo 1");
+        //Declaramos las variables
+        int num1=10;
+        int num2=5;
+        System.out.println("La primera cifra es "+num1);
+        System.out.println("La segunda cifra es "+num2);
+        int suma= num1+num2;
+        System.out.println("El resultado de su suma es "+(num1+num2));
+           System.out.println("El resultado de su suma es "+suma);
+    }
+}
